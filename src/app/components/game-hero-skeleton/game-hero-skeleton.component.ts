3451258cbf2a6f3c8859bc24a11aafa8bb700570
Alexey <hydrombee@gmail.com>
2026-08-18 22:45:53 +0200

    feat: add skeleton loading state for hero card

---
+++ src/app/components/game-hero-skeleton/game-hero-skeleton.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-hero-skeleton',
+  imports: [],
+  templateUrl: './game-hero-skeleton.component.html',
+  styleUrl: './game-hero-skeleton.component.scss'
+})
+export class GameHeroSkeletonComponent {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,14 @@ import { Component, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GameCardComponent } from '../../components/game-card/game-card.component';
 import { GameHeroComponent } from '../../components/game-hero/game-hero.component';
+import { GameHeroSkeletonComponent } from '../../components/game-hero-skeleton/game-hero-skeleton.component';
 import { GameRowComponent } from '../../components/game-row/game-row.component';
 import { Game } from '../../models/game';
 import { GamesApiService } from '../../services/games-api.service';
 
 @Component({
   selector: 'app-home',
-  imports: [AsyncPipe, GameCardComponent, GameHeroComponent, GameRowComponent],
+  imports: [AsyncPipe, GameCardComponent, GameHeroComponent, GameHeroSkeletonComponent, GameRowComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
