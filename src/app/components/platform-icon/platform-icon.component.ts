986d0e8fda1fc90a202026f18d2e930104e6f41c
Alexey <68890796+Hydrobee3000@users.noreply.github.com>
2026-08-21 23:39:28 +0200

    feat: add real brand icons to platform tags (#66)

---
+++ src/app/components/platform-icon/platform-icon.component.ts
@@ -0,0 +1,30 @@
+import { Component, input } from '@angular/core';
+import { siPlaystation, siApple, siAndroid, siLinux, siSteam } from 'simple-icons';
+
+import { getPlatformIconKind, PlatformIconKind } from '../../shared/platform-icon';
+
+const BRAND_PATHS: Partial<Record<PlatformIconKind, string>> = {
+  playstation: siPlaystation.path,
+  apple: siApple.path,
+  android: siAndroid.path,
+  linux: siLinux.path,
+  steam: siSteam.path,
+};
+
+@Component({
+  selector: 'app-platform-icon',
+  imports: [],
+  templateUrl: './platform-icon.component.html',
+  styleUrl: './platform-icon.component.scss',
+})
+export class PlatformIconComponent {
+  platform = input.required<string>();
+
+  kind(): PlatformIconKind {
+    return getPlatformIconKind(this.platform());
+  }
+
+  brandPath(): string | undefined {
+    return BRAND_PATHS[this.kind()];
+  }
+}
