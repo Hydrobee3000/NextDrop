c6775ea82aeeb66ed1fb4b67156e3be1e9159002
Alexey <68890796+Hydrobee3000@users.noreply.github.com>
2026-08-23 20:04:23 +0200

    feat: add routing between home and search pages (#80)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'search',
+    loadComponent: () => import('./pages/search/search.component').then((m) => m.SearchComponent),
+  },
+];
